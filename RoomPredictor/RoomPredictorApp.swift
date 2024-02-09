f17efa2fd745cdb7681b3e70641ebf0ec071cce8
Brayden Turner <brayden@apple.com>
2024-02-09 10:40:50 -0800

    Initial Commit

---
+++ RoomPredictor/RoomPredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomPredictorApp.swift
+//  RoomPredictor
+//
+//  Created by Brayden Turner on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomPredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
